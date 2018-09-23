67215f2e4e9ec8bf2337fb4abdd3ca3477e9ae4a
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-09-24 00:47:31 +0400

    Create play.js

---
+++ commands/music/play.js
@@ -0,0 +1,17 @@
+const Command = require('../../structures/Command');
+const fs = require('fs');
+
+module.exports = class SayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'play',
+			group: 'music',
+			memberName: 'play',
+			description: 'Searches for the provided query on YouTube.',
+			examples: ['play bag raiders shooting stars'],
+		});
+	}
+	run(msg) {
+		return;
+	}
+};
