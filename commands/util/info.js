4de0a6588fd53fee9e38f31d98cc12b3123d47d5
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-09-23 17:48:47 +0400

    Update info.js

--- commands/util/info.js
+++ commands/util/info.js
@@ -27,7 +27,7 @@ module.exports = class InfoCommand extends Command {
 			.addField('❯ Shards', this.client.options.shardCount, true)
 			.addField('❯ Commands', this.client.registry.commands.size, true)
 			.addField('❯ Home Server', this.client.options.invite ? `[Here](${this.client.options.invite})` : 'None', true)
-			..addField('❯ Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
+			.addField('❯ Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
 			.addField('❯ Uptime', moment.duration(this.client.uptime).format('hh:mm:ss', { trim: false }), true)
 			.addField('❯ Version', `v${version}`, true)
 			.addField('❯ Node Version', process.version, true)
