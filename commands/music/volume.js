5cb0d5dc891c3345abe0e6ddeeb15da86ab70c11
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-09-23 17:16:12 +0400

    Create volume.js

---
+++ commands/music/volume.js
@@ -0,0 +1,17 @@
+const Command = require('../../structures/Command');
+const fs = require('fs');
+
+module.exports = class SayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'volume',
+			group: 'music',
+			memberName: 'volume',
+			description: 'Adjusts volume for audio playback.',
+			examples: ['volume 20'],
+		});
+	}
+	run(msg) {
+		return;
+	}
+};
