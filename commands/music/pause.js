06a36cbc4769767b08aa49a032e140eb24357e3e
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-09-23 17:17:37 +0400

    Create pause.js

---
+++ commands/music/pause.js
@@ -0,0 +1,17 @@
+const Command = require('../../structures/Command');
+const fs = require('fs');
+
+module.exports = class SayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'pause',
+			group: 'music',
+			memberName: 'pause',
+			description: 'Pauses audio playback.',
+			examples: ['pause'],
+		});
+	}
+	run(msg) {
+		return;
+	}
+};
