7275978fc4bb6ed7091a612f906391a4342d9c6d
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-09-23 20:47:34 +0400

    Update gtn.js

--- commands/nsfw/gtn.js
+++ commands/nsfw/gtn.js
@@ -1,4 +1,4 @@
-const Command = require('../../structures/Command');
+const Command = require('discord.js-commando'),
 const Discord = require('discord.js');
 const snekfetch = require('snekfetch');
 const errors = require('../../assets/json/errors');
