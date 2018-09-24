71768d19063ba466b379cb90ca72bb95ce1cd6cd
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-09-24 20:47:42 +0400

    Update shop.js

--- commands/testing/shop.js
+++ commands/testing/shop.js
@@ -16,21 +16,6 @@ module.exports = class shopCommand extends Command {
         if (message.author.bot) return;
          
         let items = JSON.parse(fs.readFileSync('rpg_files/items.json', 'utf8'));
-        var array = [];
-        for (var i in items) {
-            if (items[i].purchasable) {
-                array.push({
-                    name: items[i].name,
-                    worth: items[i].worth,
-                    description: items[i].description
-                });
-            }
-        }
-        var output = "```javascript\n";
-        for (var i in array) {
-            output += "-->" + array[i].name + ": " + array[i].worth + " points\n Description: "+array[i].description + "\n";
-        }
-        output += "```";
-        message.channel.send(output);
+     
     }
 }
