97877331c79b63bf406e06dba2cf6bac744d0a48
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-09-24 19:44:43 +0400

    Create shop.js

---
+++ commands/testing/shop.js
@@ -0,0 +1,35 @@
+const Command = require('../../structures/Command');
+const fs = require("fs");
+
+module.exports = class shopCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'shop',
+            group: 'rpg',
+            memberName: 'shop',
+            description: 'displays all available items'
+        });
+    }
+
+    async run(message, args) {
+        if (message.author.bot) return;
+         
+        let items = JSON.parse(fs.readFileSync('rpg_files/items.json', 'utf8'));
+        var array = [];
+        for (var i in items) {
+            if (items[i].purchasable) {
+                array.push({
+                    name: items[i].name,
+                    worth: items[i].worth,
+                    description: items[i].description
+                });
+            }
+        }
+        var output = "```javascript\n";
+        for (var i in array) {
+            output += "-->" + array[i].name + ": " + array[i].worth + " points\n Description: "+array[i].description + "\n";
+        }
+        output += "```";
+        message.author.sendMessage(output);
+    }
+}
