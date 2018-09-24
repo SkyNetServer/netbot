ce5dedd7a16f63bfaf89e9f2b473fe650a2ec18c
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-09-24 19:31:22 +0400

    Update mine.js

--- commands/testing/mine.js
+++ commands/testing/mine.js
@@ -1,7 +1,7 @@
-const commando = require('discord.js-commando');
+const Command = require('../../structures/Command');
 const fs = require("fs");
 
-module.exports = class ButtCommand extends Command {
+module.exports = class mineCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'mine',
